8745c899647f37ab212b6f2015abd4e0c09f01ae
Then <6923403@qq.com>
2020-02-25 21:26:53 +0800

    box.cpp class

---
+++ box/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "box.h"
+using namespace std;
+
+void test1()
+{
+    Box Box1;
+    Box Box2;
+    double volume = 0.0;
+
+    Box1.setlen(1);
+    Box1.sethei(2);
+    Box1.setbre(3);
+    Box1.showre();
+}
+
+int main(int argc, char **argv)
+{
+    test1();
+}
