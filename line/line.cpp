7e0800ba00f80587ffbec882bacd5d965b716013
Then <6923403@qq.com>
2020-02-26 21:48:03 +0800

    add class line

---
+++ line/line.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "line.h"
+using namespace std;
+
+void line::setlen(double len)
+{
+    length = len;
+}
+
+double line::getlen(void)
+{
+    return length;
+}
+
+
---
+++ line/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "line.h"
+using namespace std;
+
+void test1()
+{
+    line h_line;
+    h_line.setlen(7.5);
+    cout << "Length of line: " << h_line.getlen() << endl;
+
+    h_line.length = 20.6;
+    cout << "Length of line" << h_line.length << endl;
+}
+
+int main(int argc, char *argv)
+{
+    test1();
+}
