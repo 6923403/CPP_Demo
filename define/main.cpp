3f2e68237ce80a96607809636bd907d085b811e6
Then <6923403@qq.com>
2020-03-07 19:56:34 +0800

    Define

---
+++ define/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "111" << endl;
+
+    cout << "222" << endl;
+#if 0
+    cout << "333" << endl;
+#endif
+
+    cout << "44" << endl;
+}
