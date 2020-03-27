bea33c8669af3fefd2b3fcfc800d3947d0ee882a
Then <5435@163.com>
2020-03-27 22:47:56 +0800

    9.43

--- strings/strings.cpp
+++ strings/strings.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 using namespace std;
 
-int main(int argc, char **argv)
+void test1()
 {
     const char *cp = "Hello World!!!";
     char noNull[] = {'H', 'i'};
@@ -31,4 +31,42 @@ int main(int argc, char **argv)
     vector<char> p(6, 'a');
     string p2(p.begin(), p.end());
     cout << p2 << endl;
+}
+
+void test2()
+{
+    string s = "some string", s2 = "some other string ";
+    s.insert(0, s2);
+    s2.insert(0, s2, 0, s2.size());
+    cout << s << endl;
+    cout << s2 << endl;
+}
+
+void ine(string &m, string &z1, string &z2)
+{
+    int zs1 = z1.size();
+    int zs2 = z2.size();
+    auto it1 = m.begin();
+    int ms = m.size();
+
+    for(it1; it1 <= (ms - zs1); it1++)
+    if(m.substr(it1, zs1) == z1)
+    {
+        m.erase(it1, zs1);
+        m.insert(it1, 0, z2);
+    }
+}
+
+void test3()
+{
+    string m = "71axzz5552";
+    string z1 = "axzz";
+    string z2 = "5552";
+    ine(m, z1, z2);
+    cout << m << endl;
+}
+
+int main(int argc, char **argv)
+{
+    test3();
 }
