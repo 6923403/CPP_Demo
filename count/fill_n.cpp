a69a425330903b68bc3a2209856adc59948f8ac0
Then <5435@163.com>
2020-03-30 21:59:41 +0800

    fill_n

--- count/fill_n.cpp
+++ count/fill_n.cpp
@@ -19,29 +19,42 @@ void test1()
 
 void elimDups(vector<string> &sq)
 {
+#if 0
     for(auto s1 : sq)
     {
         cout << s1 << "  ";
     }
-
+#endif
     sort(sq.begin(), sq.end());
     auto end_unique = unique(sq.begin(), sq.end());
 
-    cout << endl;
+    /*
+     * cout << endl;
     for(auto s2 : sq)
         cout << s2 << "  ";
     cout << endl;
+    */
     sq.erase(end_unique, sq.end());
 
-    for(auto s3 : sq)
-        cout << s3 << "  " << endl;
+/*    for(auto s3 : sq)
+        cout << s3 << "  " << endl;*/
 
+}
+
+bool isShorter(const string &a1, const string &a2)
+{
+    return a1.size() < a2.size();
 }
 
 void test2()
 {
     vector<string> sq = {"aaa", "ssac", "zxc", "qwe", "zws", "rtg"};
     elimDups(sq);
+    stable_sort(sq.begin(), sq.end(), isShorter);
+    for(auto s : sq)
+    {
+        cout << s << endl;
+    }
 }
 
 int main(int argc, char **argv)
