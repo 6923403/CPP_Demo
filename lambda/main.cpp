dd94100e454dc17f8708656d3e31c14014ae005a
Then <5435@63.com>
2020-03-31 09:48:02 +0800

    lambda

---
+++ lambda/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+using namespace std;
+
+void test1()
+{
+    vector<string> qq = {"asd", "xxxxx", "zqq", "qwe", "wer", "asdqw"};
+#if 0
+    stable_sort(qq.begin(), qq.end(), [](const string &s1, const string &s2)
+            {
+                return s1.size() < s2.size();
+            });
+#endif
+
+    int sz = 5;
+    partition(qq.begin(), qq.end(), [sz](string &s1){
+                if(s1.size() > sz)
+                    return true;
+                else
+                    return false;
+            });
+    for(auto s : qq)
+    {
+        cout << s << "   " << endl;
+    }
+
+}
+
+string make_plural(int q, const string &s1, const string &s2)
+{
+    return (q > 1) ? s1 + s2 : s1;
+}
+
+void test2()
+{
+    vector<string> qq = {"asd", "xxxxx", "zqq", "qwe", "wer", "asdqw", "word"};
+    int sz = 4;
+
+    auto wc = find_if(qq.begin(), qq.end(), [sz](const string &q)
+            {
+                return q.size() > sz;
+            });
+
+    auto count = qq.end() - wc;
+    cout << count << " " << make_plural(count, "word", "s") << " of length " << sz << " or longer" <<  endl;
+}
+
+int main(int argc, char **argv)
+{
+    test2();
+}
