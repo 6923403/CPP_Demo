81d3df91d279b687556e84ee9711dc546095f7ec
Then <5435@163,com>
2020-04-16 21:02:00 +0800

    json

---
+++ jsonc11-master/cmake-build-debug/CMakeFiles/jsonc11.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jsonc11.dir/bison.cc.obj"
+  "CMakeFiles/jsonc11.dir/flex.cc.obj"
+  "CMakeFiles/jsonc11.dir/json.cc.obj"
+  "CMakeFiles/jsonc11.dir/main.cpp.obj"
+  "jsonc11.exe"
+  "jsonc11.exe.manifest"
+  "jsonc11.pdb"
+  "libjsonc11.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jsonc11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
