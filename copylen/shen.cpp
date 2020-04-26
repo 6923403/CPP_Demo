5fc824badc47fa339e90949434e01173de951ba4
Then <5435@163.com>
2020-04-26 19:49:11 +0800

    copylen

--- copylen/shen.cpp
+++ copylen/shen.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <cstring>
 using namespace std;
 
 //变长数组类
@@ -59,7 +60,7 @@ int main(){
 
 
 /*
- * result: 
+ *  result: 
  *  0, 1, 2, 3, 4, 5, 6, 7, 8, 9
  *
  *  0, 1, 2, 29, 4, 100, 6, 7, 8, 9
