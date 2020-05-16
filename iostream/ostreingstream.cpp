f9604f3dcb71ac001951fbe7830bb57824d6b642
Then <5435@163.com>
2020-05-16 21:33:37 +0800

    ostringstream

--- iostream/ostreingstream.cpp
+++ iostream/ostreingstream.cpp
@@ -2,8 +2,10 @@
 #include <sstream>
 #include <fstream>
 #include <string>
+#include <vector>
 using namespace std;
 
+#if 0
 void test1()
 {
     for(const auto &entry : people)
@@ -26,8 +28,48 @@ void test1()
                 << "  invalid number(s) " << badNums.str() << endl;
     }
 }
+#endif
+
+class Personinfo
+{
+    public:
+    string name;
+    vector<string> phones;
+};
+
+void te3()
+{
+    ifstream in("in.txt");
+    ofstream outfile("out.txt", ios::app);
+
+    string line, word;
+    vector<Personinfo> people;
+
+    while(getline(in, line))
+    {
+        Personinfo info;
+        istringstream record(line);
+        record >> info.name;
+
+        while(record >> word)
+            info.phones.push_back(word);
+        people.push_back(info);
+    }
+
+    for(const auto &a : people)
+    {
+        ostringstream ostr;
+        for(const auto &b : a.phones)
+        {
+            ostr << " " << b;
+        }
+
+        outfile << a.name << " " << ostr.str() << endl;
+    }
+}
+
 
 int main(int argc, char **argv)
 {
-    test2();
+    te3();
 }
