65a5b24b434613d60038abbedf0b7f3f6bd7666f
Then <5435@163.com>
2020-05-31 16:21:34 +0800

    classDMA

---
+++ classDMA/dma.h
@@ -0,0 +1,40 @@
+#ifndef DMA_H_
+#define DMA_H_
+#include <iostream>
+
+class ABC
+{
+    private:
+        char * label;
+        int rating;
+
+    public:
+
+};
+
+class baseDMA : public ABC
+{
+    private:
+
+    public:
+        virtual View();
+        
+};
+
+class lacksDMA : public ABC
+{
+    private:
+
+    public:
+        virtual View();
+};
+
+class hasDMA : public ABC
+{
+    private:
+
+    public:
+        virtual View();
+};
+
+#endif
