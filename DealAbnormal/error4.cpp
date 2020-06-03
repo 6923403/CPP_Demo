6dc1e4382294ade53cee70670bdf72b1ca9dfbfd
Then <5435@163.com>
2020-06-04 07:57:54 +0800

    error4

---
+++ DealAbnormal/error4.cpp
@@ -0,0 +1,18 @@
+
+double hemean(double a, double b);
+double gmean(double a, double b);
+
+
+double hmean(double a, double b)
+{
+    if(a == -b)
+        throw bad_hmean(a, b);
+    return 2.0 * a * b / (a + b);
+}
+
+double gmean(double a, double b)
+{
+    if(a < 0 || b < 0)
+        throw bad_gmean(a, b);
+    return std::sqrt(a * b);
+}
