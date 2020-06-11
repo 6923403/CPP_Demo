36f522a3cd7c630be924899eb49266d5586f1c27
Then <5435@163.com>
2020-06-11 17:33:28 +0800

    student

---
+++ STU/student.h
@@ -0,0 +1,15 @@
+#ifndef STUDENT_H_
+#define STUDENT_H_
+
+class Student
+{
+    private:
+        int scor;
+        char name[3];
+        
+    public:
+        Student(const string, int scor)
+        void Menu();
+};
+
+#endif
