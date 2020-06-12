a4f5a286f72ad9b0cbaa13af1d905c25cfe79f24
Then <vcvckw@gmail.com>
2020-06-12 22:21:33 +0800

    student.cpp

--- STU/student.cpp
+++ STU/student.cpp
@@ -1,14 +1,14 @@
 #include "student.h"
 #include <iostream>
-
-using std::cout;
-using std::endl;
+//#include <vector>
+using namespace std;
 
 Student::Student()
 {
-    grade = 0;
+    //grade = 0;
+    Stu_id = 0;
+    Stu_sex = 1;
     name[0] = '\0';
-    sex = sexs(1);
 }
 
 Student::~Student()
@@ -25,4 +25,30 @@ void Student::Menu()
     cout << " * 4. 查看学生排名 " << endl;
     cout << " * 5. 其他 " << endl;
     cout << " * 6. 程序退出 " << endl;
+}
+
+void Student::Stu_os()
+{
+    string name;
+    int sex;
+    int stuid;
+    cout << "请输入学生姓名: ";
+    cin >> name;
+    cout << endl;
+    cout << "1为男 2为女" << endl;
+    cout << "请输入性别(1或2): ";
+    cin >> sex;
+    cout << endl;
+    cout << "请输入学号: ";
+    cin >> stuid;
+    cout << endl;
+
+    Stu_set(name, sex, stuid);
+}
+
+void Student::Stu_set(const string &name, const int &sex, const int &stuid)
+{
+    Stu_name.push_back(name);
+    Stu_sex = sexs(sex);
+    Stu_id = stuid;
 }
