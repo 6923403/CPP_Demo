338f8e49b259ec70069a88d7b52847fbee36b021
Then <5435@163.com>
2020-06-12 13:34:05 +0800

    student

--- STU/student.h
+++ STU/student.h
@@ -1,15 +1,28 @@
-#ifndef STUDENT_H_
-#define STUDENT_H_
+#ifndef _STUDENT_H_
+#define _STUDENT_H_
 
 class Student
 {
     private:
-        int scor;
+        int grade;
+        int sex;
         char name[3];
+        enum sexs {man = 1, woman = 2};
         
     public:
-        Student(const string, int scor)
+        Student();
+        virtual ~Student();
         void Menu();
+};
+
+class Control : public Student
+{
+    public:
+        Control();
+        ~Control();
+
+        void changestu_info();
+        void changestu_grade();
 };
 
 #endif
