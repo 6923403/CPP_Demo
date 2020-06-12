220d3c08fc34d3c0595a1572cb01136e66e57e7d
Then <vcvckw@gmail.com>
2020-06-12 22:22:32 +0800

    os_set

--- STU/student.h
+++ STU/student.h
@@ -1,18 +1,26 @@
 #ifndef _STUDENT_H_
 #define _STUDENT_H_
 
+#include <vector>
+#include <string>
+using std::string;
+
 class Student
 {
     private:
-        int grade;
-        int sex;
+        int Stu_id;
+        int Stu_sex;
+        std::vector<string> Stu_name;
         char name[3];
         enum sexs {man = 1, woman = 2};
         
     public:
         Student();
         virtual ~Student();
+
         void Menu();
+        void Stu_os();
+        void Stu_set(const string &, const int &, const int &);
 };
 
 class Control : public Student
@@ -21,8 +29,8 @@ class Control : public Student
         Control();
         ~Control();
 
-        void changestu_info();
-        void changestu_grade();
+        void changeStu_info();
+        void changeStu_grade();
 };
 
 #endif
