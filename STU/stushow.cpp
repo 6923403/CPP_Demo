dfcd7ee27fda74c412b00a7f761b00d7abf93388
Then <5435@163.com>
2020-06-12 12:49:49 +0800

    stu_show

---
+++ STU/stushow.cpp
@@ -0,0 +1,14 @@
+#include "stushow.h"
+#include <iostream>
+using std::cout;
+using std::endl;
+
+Stushow::Stushow()
+{
+    cout << " Show " << endl;
+}
+
+Stushow::~Stushow()
+{
+
+}
---
+++ STU/stushow.h
@@ -0,0 +1,13 @@
+#ifndef _STUSHOW_H_
+#define _STUSHOW_H_
+
+class Stushow
+{
+    public:
+        Stushow();
+        ~Stushow();
+        void stu_show();
+        void stusort_show();
+};
+
+#endif
