569f94b7bc67a97461862d807750002d39fd9f68
Then <5435@163.com>
2020-06-12 13:34:57 +0800

    main

--- STU/main.cpp
+++ STU/main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include "student.h"
+#include "stushow.h"
 
 int main(int argc, char **argv)
 {
-    Menu();
+    using namespace std;
 
+    Student Stu;
+    Stu.Menu();
+    int k = 0;
+    cout << "want you do? : ";
+    while(cin >> k)
+    {
+        /*
+         * 1 set stu
+         * 2 showstu
+         * 3 change stuinfo
+         * 4 show sort
+         */
+        switch(k)
+        {
+            case 1: 
+                break;
+            case 6:
+                exit(-1);
+            default: 
+                cout << "restart input" << endl;
+                break;
+        }
+        cout << "want you do? : ";
+    }
+
+    return 0;
 }
