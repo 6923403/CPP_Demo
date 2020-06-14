17ceb82b7ee88e5a03d54883420056ba65475045
Then <vcvckw@gmail.com>
2020-06-14 18:09:21 +0800

    stushow

--- STU/stushow.h
+++ STU/stushow.h
@@ -1,7 +1,10 @@
 #ifndef _STUSHOW_H_
 #define _STUSHOW_H_
 
-class Stushow
+#if 0
+#include "student.h"
+
+class Stushow : public Sutdent
 {
     public:
         Stushow();
@@ -9,5 +12,6 @@ class Stushow
         void stu_show();
         void stusort_show();
 };
+#endif
 
 #endif
