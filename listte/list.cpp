329a0a3f01e9db82ce38a1856b79c326a8d5d41b
Then <5435@163.com>
2020-06-15 12:51:19 +0800

    list

--- listte/list.cpp
+++ listte/list.cpp
@@ -1,49 +1,83 @@
 #include <iostream>
 using namespace std;
 
+void te1();
+void te2();
+
 class List
 {
     public:
+        int m_key;
         int data;
-        List * link;
+        List * node;
         List * next;
 };
 
-int create(List &L, int *data, int n)
+List createList(List *phead)
 {
-    List *q, *p;
-    int i;
-    if(n < 10)
-        return 0;
-
-    L = nullptr;
-    p = L;
-    for(i = 0; i < n; i++)
+    for(int i = 1; i< 5; i++)
     {
-
+        List *pNewNode = new List;
+        pNewNode->m_key = i;
+        pNewNode->next = nullptr;
+        phead->next = nullptr;
+        phead = pNewNode;
     }
-}
+    return *phead;
 
+}
 
 int main(int argc, char **argv)
 {
-    const a_size = 10;
-    int a[a_size], tmp;
-    cout << "insert a[]" << endl;
-    for(int i = 0; i < a_size; i++)
-    {
-        cout << "number: ";
-        cin >> tmp;
-        a[i] = tmp;
-        cout << endl;
-    }
+    te2();
+}
 
-    List *L, *head;
-    cout << "创建链表L " << endl;
-    if(create(L, a, a_size))
-    {
-        cout << "Error " << endl;
-        return -1;
-    }
+List * creat()
+{
+    List * head;
+    List * p;
+    List * q;
 
+    head = new List;
+    p = head;
+
+    for(int i = 0; i < 5; i++)
+    {
+        q = new List;
+        q->data = i;
+        cout << "q->data: " << q->data << endl;
+
+        p->next = q;
+        p = q;
+    }
+    head = head->next;
+    p->next = nullptr;
+    cout << "head->data: " << head->data << endl;
+    return head;
+}
+
+void te2()
+{
+    List * ls = creat();
+    cout << "ls->data: " << ls->data << endl;
+}
+
+void te1()
+{
+    List * head = nullptr;
+    head = new List;
+    head->m_key = 0;
+    head->next = nullptr;
+
+    *head = createList(head);
+
+    for(int i = 0; i < 5; i++)
+    {
+        if(head != nullptr)
+        {
+            cout << head->m_key << endl;
+            List * tp = head->next;
+            head = tp;
+        }
+    }
 }
