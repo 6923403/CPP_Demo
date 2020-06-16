ce1adfbf7ce070f1d3194389e56e12ea7a7c3762
Then <5435@163.com>
2020-06-16 17:22:17 +0800

    Merge list

---
+++ listte/MergeTwolist.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+    public:
+        ListNode * mergeTwoLists(ListNode * l1, ListNode * l2) {
+https://leetcode-cn.com/problems/merge-two-sorted-lists/
+        }
+};
+ 
+
+int main(int argc, char **argv)
+{
+    
+}
