23fcf57927d9572431e9e63341c4ee4d5d0b432a
Then <vcvckw@gmail.com>
2020-06-23 22:40:01 +0800

    Control

--- STU/Control.cpp
+++ STU/Control.cpp
@@ -2,12 +2,30 @@
 
 Control::Control()
 {
-
+    Stuchioce = 0;
 }
 
 Control::~Control()
 {
 
+}
+
+Control::Control(int Ch)
+{
+    Stuchioce = Ch;
+
+    Controlcase(Stuchioce);
+}
+
+Control::Controlcase(int Stuchioce)
+{
+    switch(Stuchioce)
+    {
+        case 1:
+
+            break;
+
+    }
 }
 
 void Control::changeStu_info()
