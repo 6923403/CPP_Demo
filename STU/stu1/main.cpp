3c2186f20f838607e88d7f19233121de6571d6fd
Then <vcvckw@gmail.com>
2020-06-24 18:59:36 +0800

    main

---
+++ STU/stu1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Student.h"
+using namespace std;
+
+int main() {
+    Student Stu;
+    while(true) {
+        Stu.ShowMenu();
+        cout << endl;
+    }
+    return 0;
+}
