dbf5298ceb75a9aae5bd6f565c8d4d71c37174af
Then <vcvckw@gmail.com>
2020-07-03 23:55:48 +0800

    Brass

--- brass/owbrass/brass.cpp
+++ brass/owbrass/brass.cpp
@@ -54,9 +54,27 @@ void Brass::setAccount()
     cout << endl;
 }
 
+void Brass::DrawMoney(double drawtmp)
+{
+    Balance = Balance - drawtmp;
+}
+
+double Brass::getBalance() const
+{
+    return Balance;
+}
+
 /*
  * Brass Plus
  */
+BrassPlus::BrassPlus(const string & name, long numcard, double balance)
+                     : Brass(name, numcard, balance)
+{
+    rate = 0.11125;
+    BeyondBalance = 0.0;
+    CurrentBeyond = 0.0;
+    Debt = false;
+}
 
 BrassPlus::~BrassPlus()
 {}
@@ -67,23 +85,58 @@ void BrassPlus::Deposit()
     {
         cout << "You beyond is: " << BeyondBalance << endl;
         cout << "You beyond current is: " << CurrentBeyond << endl;
+
         double m_debt = CurrentBeyond - BeyondBalance;
         cout << "You have return Beyond is: " << m_debt << endl;
-        /*
-         * cout << "You also need to pay interest according to the interest rate: " << [m_debt, this->rate](double m_rate) -> double {
+      /* 
+        cout << "You also need to pay interest according to the interest rate: " << [m_debt, this->rate](double m_rate) -> double {
             return m_rate = m_debt * this->rate;
         } << endl;
         */
 
         cout << "You return after ." << endl;
-        cout << "Current Balance: " << br->Balance << endl;
-        cout << "Current BeyondBalancei: " << br->CurrentBeyond << endl;
+        Brass::Show();
+//        cout << "Current Balance: " << br->Balance << endl;
+        cout << "Current BeyondBalancei: " << CurrentBeyond << endl;
     }
     else
     {
-        cout << "Balance: " << Balance << endl;
+        //cout << "Balance: " << br->Balance << endl;
+        Brass::Show();
         cout << "CurrentBeyond: " << CurrentBeyond << "  " << "You not have Debt ." << endl;
-
     }
+}
 
+void BrassPlus::DrawMoney()
+{
+    double batmp = Brass::getBalance(); 
+    cout << "You Can Draw: " << (batmp + CurrentBeyond)<< endl; 
+    double drawtmp;
+    cout << "You need Num: ";
+    cin >> drawtmp;
+    if(drawtmp <= (batmp + CurrentBeyond))
+    {
+        if(drawtmp <= batmp)
+        {
+            Brass::DrawMoney(drawtmp);
+        }
+        else
+        {
+            drawtmp = drawtmp - batmp;
+            Brass::DrawMoney(batmp);
+            DrawMoney(drawtmp);
+        }
+    }
+}
+
+void BrassPlus::DrawMoney(double drawtmp)
+{
+    CurrentBeyond = CurrentBeyond - drawtmp;
+}
+
+void BrassPlus::Show() const
+{
+    Brass::Show();
+    cout << "BeyondBalance sum: " << BeyondBalance << endl;
+    cout << "CurrentBeyond: " << CurrentBeyond << endl;
 }
