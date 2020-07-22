7f6840da3125db363d34512b7d9c365ce490a695
Then <vcvckw@gmail.com>
2020-07-22 18:01:27 +0800

    Sqtack

--- stack/Sqstack.cpp
+++ stack/Sqstack.cpp
@@ -14,6 +14,11 @@ typedef struct
 
 int initStack(SqSrack &s, int size, int inc);
 int Push_sq(SqSrack &s, int e);
+void showSq(SqSrack s);
+int Stack_empty(SqSrack s);
+int Pop_Sq(SqSrack s);
+void ClearStack(SqSrack &s);
+
 
 int Push_sq(SqSrack &s, int e)
 {
@@ -41,15 +46,46 @@ int initStack(SqSrack &s, int size, int inc)
     return 1;
 }
 
+int Stack_empty(SqSrack s)
+{
+    if(s.top == 0)
+        return 1;
+
+    return 0;
+}
+
+int Pop_Sq(SqSrack s)
+{   
+    return s.elem[s.top - 1];
+}
+
+void ClearStack(SqSrack &s)
+{
+    if (s.top == 0)
+        return;
+    s.size = 0;
+    s.top = 0;
+
+    cout << "Clear_Stack ." << endl;
+}
+
+void showSq(SqSrack s)
+{
+    while(s.top-- != -1)
+    {
+        cout << s.elem[s.top];
+    }
+}
+
 int main(int argc, char **argv)
 {
     SqSrack s;
 
-    int size, increment, i;
+    int size, increment;
 
     size = max_size;
     increment = max_size;
-    int e, eArr[5] = {1, 2, 3, 4, 5};
+    int eArr[5] = {1, 2, 3, 4, 5};
     
     if(!initStack(s, size, increment))
     {
@@ -57,13 +93,41 @@ int main(int argc, char **argv)
         exit(0);
     }
 
-    //push
+    /*
+     * push stack
+     */
     for(int i = 0; i < max_size; ++i)
     {
         if(!Push_sq(s, eArr[i]))
         {
-            cout << "Error = push_q" << endl;
+            cout << "Error = push_sq" << endl;
             exit(0);
-/bin/bash: k：未找到命令
+        }
     }
+
+    /*
+     * Stack Full
+     */
+    if(Stack_empty(s))
+        cout << "Stak is full ." << endl;
+    else
+        cout << "Stack is NULL ." << endl;
+
+    /*
+     * Pop stack
+     */
+    cout << "Pop_Sq = " << Pop_Sq(s) << endl;
+
+    /*
+     * show stack
+     */
+    showSq(s);
+
+    /*
+     * Clear Stack
+     */
+    cout << endl;
+    ClearStack(s);
+
+    return 0;
 }
