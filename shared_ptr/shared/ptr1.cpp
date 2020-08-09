d86220c74d7855a4de8765193cb42481684acb48
vcvc <vcvckw@gmail.com>
2020-08-09 22:01:02 +0800

    shared_ptr

--- shared_ptr/shared/ptr1.cpp
+++ shared_ptr/shared/ptr1.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <list>
 #include <memory>
 
 int main(int argc, char **argv)
 {
-    shared_ptr<string> p1;
-    shared_ptr<list<int>> p2;
+    std::shared_ptr<std::string> p1;
+    std::shared_ptr<std::list<int>> p2;
 
     if(p1 && p1->empty())
     {
         *p1 = "hi";
     }
 
-    shared_ptr<int> p3 = make_shared<int>(42);
-    shared_ptr<string> p4 = make_shared<string>(10, '9');
-    shared_ptr<int> p5 = make_shared<int>();
+    std::shared_ptr<int> p3 = std::make_shared<int>(42);
+    std::shared_ptr<std::string> p4 = std::make_shared<std::string>(10, '9');
+    std::shared_ptr<int> p5 = std::make_shared<int>();
 
     //p6 allocate vector<string>
-    auto p6 = make_shared<vector<string>>();
+    auto p6 = std::make_shared<std::vector<std::string>>();
 }
