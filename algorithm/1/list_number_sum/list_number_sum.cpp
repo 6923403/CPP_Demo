bc5c07ab59c2031d2f6881385e486f152fddc0ec
vcvc <vcvckw@gmail.com>
2020-09-12 22:57:37 +0800

    list_num_sum

---
+++ algorithm/1/list_number_sum/list_number_sum.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode * next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+ListNode * createlist()
+{
+    ListNode * l = new ListNode(0);
+    ListNode * p = l;
+
+    int t = 0;
+    for(int i = 0; i < 4; i++)
+    {
+        ListNode * newNode = new ListNode(0);
+        cin >> t;
+        newNode->val = t;
+        newNode->next = NULL;
+
+        p->next = newNode;
+        p = newNode;
+    }
+
+    p->next = NULL;
+    return l->next;
+}
+
+ListNode * twosum(ListNode * l1, ListNode * l2)
+{
+    int ten = 0;
+    int sum = 0;
+
+    ListNode *res = new ListNode(0);
+    ListNode *p = res;
+
+    while(l1 || l2 || ten != 0)
+    {
+        sum = 0;
+        sum += ten;
+        ten = 0;
+
+        if(l1 && l2)
+        {
+            sum += l1->val + l2->val;
+            l1 = l1->next;
+            l2 = l2->next;
+        }
+        else if(l1)
+        {
+            sum += l1->val;
+            l1 = l1->next;
+        }
+        else if(l2)
+        {
+            sum += l2->val;
+            l2 = l2->next;
+        }
+
+        p->val = sum % 10;
+        ten = sum / 10;
+
+        if(l1 || l2 || ten != 0)
+        {
+            ListNode * newnode = new ListNode(0);
+            newnode->next = NULL;
+            p->next = newnode;
+            p = newnode;
+        }
+    }
+    p->next = NULL;
+    return res;
+}
+
+void showlist(ListNode *l1)
+{
+    for(int i = 0; i < 4; i++)
+    {
+        cout << l1->val << endl;
+        l1 = l1->next;
+    }
+}
+
+int main()
+{
+    ListNode *rec = new ListNode(0);
+
+    cout << "create list1" << endl;
+    ListNode *l1 = createlist();
+
+    cout << "create list2" << endl;
+    ListNode *l2 = createlist();
+    rec = twosum(l1, l2);
+    cout << "show list1" << endl;
+    showlist(l1);
+}
