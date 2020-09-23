84a272c126fd439a4c55bd3828de3ece15331454
vcvc <vcvckw@gmail.com>
2020-09-23 17:34:21 +0800

    strchr

---
+++ function/strchr.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main ()
+{
+   const char str[] = "werkwnrkqwnrknwenr/root/git/CPP_Demo/function";
+   const char ch = '/';
+   char *ret;
+
+   ret = strchr((char *)str, ch);
+
+   //printf("|%c| 之后的字符串是 - |%s|\n", ch, ret);
+   printf("%s  \n", ret);
+   
+   return(0);
+}
