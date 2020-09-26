f96474169d3bc7afd8b668aba9f29c900a5365cd
vcvc <vcvckw@gmail.com>
2020-09-26 23:41:29 +0800

    binary_tree_trave

---
+++ algorithm/1/binary_traversal.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    public:
+        vector<int> preorderTraversal(TreeNode* root) {
+
+            vector<int> res;
+            tree(root, res);
+
+            return res; //Happy AC
+        }
+
+        //前序遍历
+        void tree(TreeNode *root, vector<int> &res)
+        {
+            if(root == nullptr)
+                return;
+            res.push_back(root->val);
+            tree(root->left, res);
+            tree(root->right, res);
+        }
+
+        //中序遍历
+        void tree(TreeNode *root, vector<int> &res)
+        {
+            if(root == nullptr)
+                return;
+            tree(root->left, res);
+            res.push_back(root->val);
+            tree(root->right, res);
+        }
+
+        //后序遍历
+        void tree(TreeNode *root, vector<int> &res)
+        {
+            if(root == nullptr)
+                return;
+            tree(root->left, res);
+            tree(root->right, res);
+            res.push_back(root->val);
+        }
+
+};
