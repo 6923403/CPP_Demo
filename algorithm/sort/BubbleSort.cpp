11d7ae173312f2c8b8d3c30f71f3aea0323d230b
vcvc <vcvckw@gmail.com>
2020-10-03 23:26:38 +0800

    bubble

--- algorithm/sort/BubbleSort.cpp
+++ algorithm/sort/BubbleSort.cpp
@@ -10,23 +10,34 @@ void bubble_sort1(vector<int> vec)
     {
         for(int j = 0; j < len - i - 1; j++)
         {
-            if(vec[j] > v[j + 1])
-                swap(vec[j], v[j + 1]);
+            if(vec[j] > vec[j + 1])
+                swap(vec[j], vec[j + 1]);
         }
+    }
+
+    for(auto &s : vec)
+    {
+        cout << s << endl;
     }
 }
 
 template<typename T>
-void bubble_sort2(T arr[], int len)
+void bubble_sort2(T &arr)
 {
-    int n = end(arr) - begin(arr);
-    for(int i = 0, i < n; i++)
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    for(int i = 0; i < n; i++)
     {
         for(int j = 0; j < n - i - 1; j++)
         {
-            if(arr[j] < arr[j + 1])
+            if(arr[j] > arr[j + 1])
                 swap(arr[j], arr[j + 1]);
         }
+    }
+
+    for(int k = 0; k < n; k++)
+    {
+        cout << arr[k] << endl;
     }
 }
 
@@ -34,6 +45,10 @@ int main(int argc, char **argv)
 {
     vector<int> vec = {12, 23, 32, 11, 33, 21, 43, 10};
     bubble_sort1(vec);
-    bubble_sort2(vec);
-    bubble_sort2(vec);
+
+    cout << "222" << endl;
+    int arr[] = {12, 32, 30, 21, 33, 44, 55};
+    bubble_sort2(arr);
+
+    return 0;
 }
