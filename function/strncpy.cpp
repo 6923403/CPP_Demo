49bdbe736e81e9de15d3b339ddfe2ca4f12a6390
Vcvc <vcvckw@gmail.com>
2020-12-02 21:56:38 +0800

    strncpy

--- function/strncpy.cpp
+++ function/strncpy.cpp
@@ -13,6 +13,9 @@ int main()
     memset(dest, '\0', sizeof(dest));
     strcpy(src, "Relax mentality");
     strncpy(dest, src, 5);
+    /*
+     * 最终的目标字符串： Relax
+     */
     printf("最终的目标字符串： %s\n", dest);
 
     return 0;
