5eab4ac89afea5db520818d3bb0af86453ff7a0b
Vcvc <vcvckw@gmail.com>
2020-12-27 20:17:33 +0800

    va_start

---
+++ function/va_start.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+using namespace std;
+
+int sum(int, ...);
+
+int main(int argc, char **argv)
+{
+    printf("10、20 和 30 的和 = %d\n",  sum(3, 10, 20, 30) );
+    printf("4、20、25 和 30 的和 = %d\n",  sum(4, 4, 20, 25, 30) );
+
+    return 0;
+}
+
+int sum(int num_args, ...)
+{
+   int val = 0;
+   va_list ap;
+   int i;
+
+   va_start(ap, num_args);
+   for(i = 0; i < num_args; i++)
+   {
+      val += va_arg(ap, int);
+   }
+   va_end(ap);
+ 
+   return val;
+}
