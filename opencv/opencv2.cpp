03c6afc715bff703412ca4658e4e225bf8ccd577
Vcvc <vcvckw@gmail.com>
2021-01-07 23:45:50 +0800

    opencv2

--- opencv/opencv2.cpp
+++ opencv/opencv2.cpp
@@ -4,19 +4,28 @@
 using namespace std;
 using namespace cv;
 
-int main(int argc, char **argv)
-{
-    if(argc < 2)
+void k1(const Mat& src, Mat& dst);
+void k2(const Mat& src, Mat& dst);
+
+int main(int argc, char **argv) {
+    if (argc < 2)
         exit(1);
     Mat src, dst;
     src = imread(argv[1]);
-    if(!src.data)
-    {
+    if (!src.data) {
         cout << "Not jpg" << endl;
         exit(0);
     }
+    //k1(src, dst); //图片锐化
+    k2(src, dst); //边缘处理
     imshow("img", src);
+    imshow("image_process_ker", dst);
+    waitKey(0);
+    return 0;
+}
 
+void k1(const Mat& src, Mat& dst)
+{
     Mat kernel(3,3,CV_32F,Scalar(0));
     kernel.at<float>(1,1) = 5;
     kernel.at<float>(0,1) = -1;
@@ -28,8 +37,18 @@ int main(int argc, char **argv)
     dst.row(dst.rows-1).setTo(Scalar(0,0,0));
     dst.col(0).setTo(Scalar(0,0,0));
     dst.col(dst.cols-1).setTo(Scalar(0,0,0));
-    imshow("image_process_ker", dst);
-    waitKey(0);
+}
 
-    return 0;
+void k2(const Mat& src, Mat& dst)
+{
+    Mat kernel(2, 2, CV_32F, Scalar(0));
+    kernel.at<float>(0, 0) = 1;
+    kernel.at<float>(0, 1) = 0;
+    kernel.at<float>(1, 0) = 0;
+    kernel.at<float>(1, 1) = -1;
+    filter2D(src, dst, src.depth(), kernel);
+    /*
+    dst.row(0).setTo(Scalar(0, 0));
+    dst.row(dst.).setTo(Scalar(0, 0));
+    */
 }
