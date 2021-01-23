17ed3eddc7d40b54adcf799dbb48d5b96d68e725
Vcvc <vcvckw@gmail.com>
2021-01-23 20:10:25 +0800

    getcwd

--- getcwd/getcwd.cpp
+++ getcwd/getcwd.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <cstdlib>
 #include <unistd.h>
 
 int main(int argc, char** argv)
 {
     char* strdir = getcwd(nullptr, 256);
     std::cout << "strdir = " << strdir << std::endl;
+
+    strncat(strdir, "./resources", 16);
+    std::cout << "strdir2 = " << strdir << std::endl;
 
     return 0;
 }
