4802953b61451989b79989a51015565a5279a74f
Vcvc <vcvckw@gmail.com>
2021-03-10 23:37:07 +0800

    lock

---
+++ pthread/lock_class/lock_class.h
@@ -0,0 +1,115 @@
+#ifndef CPP_DEMO_LOCK_CLASS_H
+#define CPP_DEMO_LOCK_CLASS_H
+
+#include <exception>
+#include <pthread.h>
+#include <semaphore.h>
+
+class sem
+{
+public:
+    sem()
+    {
+        /*
+         * 创建并初始化信号量
+         */
+        if(sem_init(&m_sem, 0, 0) != 0)
+        {
+            throw std::exception();
+        }
+    }
+
+    ~sem()
+    {
+        sem_destroy(&m_sem);
+    }
+
+    bool wait()
+    {
+        return sem_wait(&m_sem) == 0;
+    }
+
+    /*
+     * 增加信号量
+     */
+    bool post()
+    {
+        return sem_post(&m_sem) == 0;
+    }
+
+private:
+    sem_t m_sem;
+};
+
+class locker {
+public:
+    locker() {
+        if (pthread_mutex_init(&m_mutex, NULL) != 0) {
+            throw std::exception();
+        }
+    }
+
+    ~locker() {
+        pthread_mutex_destroy(&m_mutex);
+    }
+
+    bool lock()
+    {
+        return pthread_mutex_lock(&m_mutex, NULL) == 0;
+    }
+
+    bool unlock()
+    {
+        return pthread_mutex_unlock(&m_mutex, NULL) == 0;
+    }
+
+private:
+    pthread_mutex_t m_mutex;
+};
+
+class cond
+{
+public:
+    cond()
+    {
+        if(pthread_mutex_init(&m_mutex, NULL) == 0)
+        {
+            throw std::exception();
+        }
+
+        if(pthread_cond_init(&m_cond, NULL) == 0)
+        {
+            /*
+             * 如果出现问题 释放已经占用的资源
+             */
+            pthread_mutex_destroy(&m_mutex);
+            throw std::exception();
+        }
+    }
+
+    ~cond()
+    {
+        pthread_mutex_destroy(&m_mutex);
+        pthread_cond_destroy(&m_cond);
+    }
+
+    bool wait()
+    {
+        int ret = 0;
+        pthread_mutex_lock(&m_mutex);
+        ret = pthread_cond_wait(&m_cond, &m_mutex);
+        pthread_mutex_unlock(&m_mutex);
+        return ret == 0;
+    }
+
+    bool siganl()
+    {
+        return pthread_cond_signal(&m_cond) == 0;
+    }
+
+private:
+    pthread_mutex_t m_mutex;
+    pthread_cond_t m_cond;
+};
+
+#endif //CPP_DEMO_LOCK_CLASS_H
