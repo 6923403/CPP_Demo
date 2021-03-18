cf2a4bd33e8bd525bc6711f9cce3e25d52efc46e
Vcvc <vcvckw@gmail.com>
2021-03-18 23:48:41 +0800

    init

--- pthread/pool1/main.cpp
+++ pthread/pool1/main.cpp
@@ -4,7 +4,19 @@ int main(int argc, char** argv)
 {
     Pool pool;
     pool.init(5);
+    Task* task = new Task[5];
+    bool flag = false;
 
+    for(int i = 0; i < 5; i++)
+    {
+        pool.add_pool(&task[i]);
+        flag = pool.back_destroy();
+        if(flag)
+            break;
+    }
+
+
+    delete[] task;
     std::cout << "操作完成" << std::endl;
 
     return 0;
