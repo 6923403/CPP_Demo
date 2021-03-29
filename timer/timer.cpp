cc9343b6c9ab852a810993afbc119784bb7c0ca0
Vcvc <vcvckw@gmail.com>
2021-03-29 22:24:52 +0800

    timer

--- timer/timer.cpp
+++ timer/timer.cpp
@@ -2,7 +2,6 @@
 
 Timer::Timer() : m_expired(true), m_try_to_expire(false)
 {
-
 }
 
 Timer::~Timer()
@@ -15,3 +14,164 @@ Timer::Timer(const Timer& timer)
     m_expired = timer.m_expired.load();
     m_try_to_expire = timer.m_try_to_expire.load();
 }
+
+sort_timer_list::sort_timer_list() : head(nullptr), tail(nullptr)
+{
+}
+
+sort_timer_list::~sort_timer_list()
+{
+    Timer* tmp = nullptr;
+    tmp = head;
+
+    while(tmp)
+    {
+        head = tmp->next;
+        delete tmp;
+        tmp = head;
+    }
+}
+
+void sort_timer_list::add_timer(Timer* timer)
+{
+    if(!timer)
+    {
+        return;
+    }
+
+    if(!head)
+    {
+        head = tail = timer;
+        return;
+    }
+
+    if(timer->expire < head->expire)
+    {
+        timer->next = head;
+        head->prev = timer;
+        head = timer;
+        return;
+    }
+
+    add_timer(timer, head);
+}
+
+void sort_timer_list::adjust_timer(Timer* timer)
+{
+    if(!timer)
+    {
+        return;
+    }
+
+    Timer* tmp = nullptr;
+    timer = tmp->timer->next;
+
+    if(!timer || (timer->expire < tmp->expire))
+    {
+        return;
+    }
+
+    if(timer == head)
+    {
+        head = head->next;
+        head->prev = nullptr;
+        timer->next = nullptr;
+        add_timer(timer, head);
+    }
+    else
+    {
+        /*
+         * 如果不是链表头节点 则将该定时器从链表中取出 然后插入所在位置之后的部分链表中
+         */
+        timer->prev->next = timer->next;
+        timer->next->prev = timer->prev;
+        add_timer(timer, timer->next);
+    }
+}
+
+void sort_timer_list::del_timer(Timer* timer)
+{
+    if(!timer)
+    {
+        return;
+    }
+
+    if((timer == head) && (timer == tail))
+    {
+        delete timer;
+        head = nullptr;
+        tail = nullptr;
+        return;
+    }
+
+    if(timer == head)
+    {
+        head = head->next;
+        head->prev = nullptr;
+        delete timer;
+        return;
+    }
+
+    if(timer == tail)
+    {
+        tail = tail->prev;
+        tail->next = nullptr;
+        delete timer;
+        return;
+    }
+
+    timer->prev->next = timer->next;
+    timer->next->prev = timer->prev;
+    delete timer;
+}
+
+void sort_timer_list::tick()
+{
+    if(!head)
+    {
+        return;
+    }
+
+    std::cout << "timer tick " << std::endl;
+    time_t cur = time(NULL);
+    Timer* tmp = head;
+
+    while(tmp)
+    {
+        if(cur < tmp->expire)
+        {
+            break;
+        }
+
+        /*
+         * 调用定时器回调函数 以执行定时任务
+         */
+        tmp->cb_func(tmp->user_data);
+        head = tmp->next;
+
+        if(head)
+        {
+            head->prev = nullptr;
+        }
+
+        delete tmp;
+        tmp = head;
+    }
+}
+
+
+void sort_timer_list::add_timer(Timer* timer, Timer* head)
+{
+    sort_timer_list* prev = lst_head;
+    sort_timer_list* tmp = prev->next;
+
+    while(tmp)
+    {
+        if(timer->expire < tmp->expire)
+        {
+
+        }
+    }
+}
+
+
--- timer/timer.h
+++ timer/timer.h
@@ -1,6 +1,7 @@
 #ifndef CPP_DEMO_TIMER_H
 #define CPP_DEMO_TIMER_H
 
+#include <iostream>
 #include <functional>
 #include <chrono>
 #include <thread>
@@ -13,11 +14,31 @@
 //https://blog.csdn.net/zang141588761/article/details/85600194
 //https://blog.csdn.net/u012234115/article/details/89857431
 
+const int BUFFER_SIZE = 64;
+
+class Timer;
+
+struct client_data
+{
+    sockaddr_in addr;
+    int sockfd;
+    char buf[BUFFER_SIZE];
+    Timer* timer;
+};
+
 class Timer {
 public:
     Timer();
     Timer(const Timer& timer);
     ~Timer();
+
+public:
+    void (*cb_func)(client_data*);
+
+    client_data* user_data;
+    time_t expire; //超时时间
+    Timer* prev;
+    Timer* next;
 
 private:
     std::mutex m_mutex;
@@ -26,5 +47,25 @@ private:
     bool m_try_to_expire;
 };
 
+class sort_timer_list
+{
+public:
+    sort_timer_list();
+    ~sort_timer_list();
+
+    void add_timer(Timer* timer);
+    void adjust_timer(Timer* timer);
+    void del_timer(Timer* timer);
+    void tick();
+
+public:
+    void add_timer(Timer* timer, Timer* head);
+
+    sort_timer_list* head;
+    sort_timer_list* tail;
+
+private:
+
+};
 
 #endif //CPP_DEMO_TIMER_H
