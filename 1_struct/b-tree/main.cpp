5a4d2cf43afed5c95d77c5ddc442ba5e64d831fd
Vcvc <vcvckw@gmail.com>
2021-05-21 22:38:46 +0800

    init

---
+++ 1_struct/b-tree/main.cpp
@@ -0,0 +1,15 @@
+#include "bt.h"
+
+//https://blog.csdn.net/alzzw/article/details/97663352
+
+int main(int argc, char** argv)
+{
+    BTree bt;
+    std::vector<int > a = {3, 5, 7, 9, 11, 44, 55, 22, 25};
+    int asize = a.size();
+    for(int i = 0; i < asize; i++) {
+        bt.create_tree(a[i]);
+    }
+
+    return 0;
+}
