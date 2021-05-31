f1b978e01714b18c4bf13b1f18a4e69849719d87
Vcvc <vcvckw@gmail.com>
2021-05-31 23:37:30 +0800

    sql

--- mysql/sql/Connect_Sql.cpp
+++ mysql/sql/Connect_Sql.cpp
@@ -1,21 +1,23 @@
 #include "Connect_Sql.h"
 
-Connect_Sql::Connect_Sql() : mysql(nullptr)
+Connect_Sql::Connect_Sql() : mysql(NULL)
 {
 }
 
 Connect_Sql::~Connect_Sql()
 {
+    mysql_close(mysql);
 }
 
-bool Connect_Sql::connect_mysql(std::string addr, std::string username, std::string password, int port)
+bool Connect_Sql::connect_mysql(std::string addr, std::string db_name, std::string username, std::string password, int port)
 {
-    while(!mysql)
+    mysql = mysql_init(mysql);
+    if(!mysql)
     {
-        mysql = mysql_init(mysql);
+        return false;
     }
 
-    mysql = mysql_real_connect(mysql, addr.c_str(), username.c_str(), password.c_str(), "servert", port, NULL, 0);
+    mysql = mysql_real_connect(mysql, addr.c_str(), username.c_str(), password.c_str(), db_name.c_str(), port, NULL, 0);
 
     if(!mysql)
     {
@@ -23,5 +25,14 @@ bool Connect_Sql::connect_mysql(std::string addr, std::string username, std::str
         return false;
     }
 
+    if(mysql_set_character_set(mysql, "utf8"))
+    {
+        return false;
+    }
+
     return true;
+}
+
+bool Connect_Sql::insert_data(std::map<std::string, std::string> m_user)
+{
 }
--- mysql/sql/Connect_Sql.h
+++ mysql/sql/Connect_Sql.h
@@ -1,8 +1,11 @@
-#ifndef MYSQL_CONNECT_SQL_H
-#define MYSQL_CONNECT_SQL_H
+#ifndef CONNECT_SQL_H
+#define CONNECT_SQL_H
 
 #include <iostream>
 #include <string>
+#include <unistd.h>
+#include <vector>
+#include <map>
 #include <mysql/mysql.h>
 
 class Connect_Sql
@@ -11,10 +14,11 @@ public:
     Connect_Sql();
     ~Connect_Sql();
 
-    bool connect_mysql(std::string addr, std::string username, std::string password, int port);
+    bool connect_mysql(std::string addr, std::string db_name, std::string username, std::string password, int port);
+    bool insert_data(std::map<std::string, std::string> m_user);
 
 private:
     MYSQL* mysql;
 };
 
-#endif //MYSQL_CONNECT_SQL_H
+#endif //CONNECT_SQL_H
