a3a102ba54e89b70c1bd6fec08c8f17b43d50d7d
Vcvc <vcvckw@gmail.com>
2021-06-02 18:55:37 +0800

    init

--- mysql/main.cpp
+++ mysql/main.cpp
@@ -64,34 +64,39 @@ int main(int argc, char** argv)
     Connect_Sql sql;
     if(sql.connect_mysql(ip_addr, db_name, username, password, port))
     {
-        std::cout << "123" << std::endl;
+        std::cout << "连接数据库" << std::endl;
     }
     else
     {
         std::cout << "连接数据库出错" << std::endl;
     }
 
-    std::map<std::string, std::string> m_user;
-    std::string n, q;
-    std::cout << "用户名+密码" << std::endl;
-    std::cin >> n >> q;
-    m_user.insert(std::pair<std::string, std::string>(n, q));
+//    std::map<std::string, std::string> m_user;
+//    std::string n, q;
+//    std::cout << "用户名+密码" << std::endl;
+//    std::cin >> n >> q;
+//    m_user.insert(std::pair<std::string, std::string>(n, q));
+//
+//    if(sql.insert_data(m_user))
+//    {
+//        std::cout << "数据插入" << std::endl;
+//    }
+//    else
+//    {
+//        std::cout << __LINE__ << " " << " insert error" << std::endl;
+//    }
+//
+//    std::string data_username;
+//    std::cout << "输入要删除的用户" << std::endl;
+//    std::cin >> data_username;
+//    if(sql.delete_data(data_username))
+//    {
+//        std::cout << "删除用户: " << data_username << " 成功" << std::endl;
+//    }
 
-    if(sql.insert_data(m_user))
+    if(!sql.search_data())
     {
-        std::cout << "数据插入" << std::endl;
-    }
-    else
-    {
-        std::cout << __LINE__ << " " << " insert error" << std::endl;
-    }
-
-    std::string data_username;
-    std::cout << "输入要删除的用户" << std::endl;
-    std::cin >> data_username;
-    if(sql.delete_data(data_username))
-    {
-        std::cout << "删除用户: " << data_username << " 成功" << std::endl;
+        std::cout << "查询失败" << std::endl;
     }
 
     return 0;
