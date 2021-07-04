5e68ab0c14d3ff12e4ad238d8e47b3baa38d83aa
Vcvc <vcvckw@gmail.com>
2021-07-05 00:15:12 +0800

    client

--- linux_socket_high/p8_sendfile/client.cpp
+++ linux_socket_high/p8_sendfile/client.cpp
@@ -12,11 +12,18 @@ int main()
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
     addr.sin_port = htons(20999);
-    addr.sin_addr = inet_addr("192.168.1.17");
+    addr.sin_addr.s_addr = inet_addr("192.168.1.17");
 
-    const int buf_size = 2048;
+    const int buf_size = 10240;
     char buf[buf_size] = {0};
-    memset(buf, 0, 20)
+    memset(buf, 0, buf_size);
+
+    int ret = connect(sockfd, (struct sockaddr*) &addr, sizeof(addr));
+    assert(ret == 0);
+
+    while(recv(sockfd, buf, buf_size, 0) > 0){};
+
+    std::cout << buf << std::endl;
 
     return 0;
 }
