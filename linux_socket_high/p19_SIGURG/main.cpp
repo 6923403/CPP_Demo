786ccee8f79f7d62f901e429966c21cdaf65253d
Vcvc <vcvckw@gmail.com>
2021-07-15 00:02:10 +0800

    sig

---
+++ linux_socket_high/p19_SIGURG/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <unistd.h>
+#include <signal.h>
+#include <cstring>
+#include <cassert>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <errno.h>
+
+const int MAX_BUFF_SIZE = 1024;
+static int connfd;
+
+void sig_urg(int sig)
+{
+    int save_errno = errno;
+    char buf[MAX_BUFF_SIZE];
+    memset(&buf, '\0', sizeof(buf));
+
+    int ret = recv(connfd, buf, MAX_BUFF_SIZE - 1, MSG_OOB);
+    std::cout << "recv: " << buf << std::endl;
+
+
+    errno = save_errno;
+}
+
+void addsig(int sig, void (*sig_handler)(int))
+{
+    struct sigaction sa;
+    memset(sa, '\0', sizeof(sa));
+
+    sa.sa_handler = sig_handler;
+    sa.sa_flags |= SA_RESTART;
+
+    sigfillset(&sa.sa_mask);
+    assert(sigaction(sig, &sa, NULL) != -1);
+}
+
+int main(int argc, char **argv)
+{
+    int port = 20999;
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if((bind(sockfd, (struct sockaddr*)&address, sizeof(address)) == -1) || (listen(sockfd, 5) == -1) )
+    {
+        exit(0);
+    }
+
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+
+    connfd = accept(sockfd, (struct sockadr*) &client_addr, &client_addr_len);
+    assert(connfd != -1);
+
+    addsig(SIGURG, sig_urg);
+
+    return 0;
+}
