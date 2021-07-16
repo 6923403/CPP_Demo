143185836fc67806fae4357f2f7985b254ed3fe9
Vcvc <vcvckw@gmail.com>
2021-07-16 23:53:28 +0800

    init

--- linux_socket_high/p21_SIGALRM/main.cpp
+++ linux_socket_high/p21_SIGALRM/main.cpp
@@ -30,13 +30,39 @@ int setnonblocking(int fd)
 
 }
 
-void addfd(iint epollfd, int fd)
+void addfd(int epollfd, int fd)
 {
     epoll_event event;
     event.data.fd = fd;
     event.events = EPOLLIN | EPOLLET;
     epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &event);
     setnonblocking(fd);
+}
+
+void sig_handler(int sig)
+{
+    int error = errno;
+    int msg = sig;
+    send(pipefd[1], (char*)&msg, 1, 0);
+    errno = error;
+}
+
+void addsig(int sig)
+{
+    struct sigaction sa;
+    sa.sa_handler = sig_handler;
+    sa.sa_flags |= SA_RESTART;
+
+    sigfillset(&sa.sa_mask);
+    assert(sigaction(sig, &sa, NULL) != -1);
+}
+
+void timer_handler()
+{
+    timer_lst.tick();
+
+    //因为一次alarm调用只会触发一次，所以要重新设定
+    alarm(TIMESLOT);
 }
 
 void cb_func(client_data *user_data) //定时器回调函数
