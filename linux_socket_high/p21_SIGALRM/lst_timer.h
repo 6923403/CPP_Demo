803890703d636bb78dd7f84366cb36a89bd11411
Vcvc <vcvckw@gmail.com>
2021-07-17 23:31:01 +0800

    timer

--- linux_socket_high/p21_SIGALRM/lst_timer.h
+++ linux_socket_high/p21_SIGALRM/lst_timer.h
@@ -15,7 +15,7 @@ struct client_data
 {
     sockaddr_in address;
     int sockfd;
-    char buffer[BUFFER_SIZE];
+    char buf[BUFFER_SIZE];
     util_timer *timer;
 };
 
@@ -25,9 +25,8 @@ public:
     util_timer() : prev(nullptr), next(nullptr)
     {}
 
-private:
     time_t expire;
-    void *(cb_func) (client_data);
+    void (*cb_func) (client_data *);
     client_data *user_data;
     util_timer *prev;
     util_timer *next;
