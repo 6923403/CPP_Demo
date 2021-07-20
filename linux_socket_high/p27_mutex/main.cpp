ed3a35d5cbb85f7cbbcb1c742e097b3b06886160
Vcvc <vcvckw@gmail.com>
2021-07-20 23:42:35 +0800

    init

---
+++ linux_socket_high/p27_mutex/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <unistd.h>
+
+#include "Locker.h"
+
+static int n = 1;
+static Sem m_sem;
+static Cond m_cond;
+static Locker lock;
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
